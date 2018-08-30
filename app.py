ca1a3b2442d54e1e2b199c936e0d1bba80790c6a
nhung do <dothinhung.hanu97@gmail.com>
2018-08-30 21:50:01 +0700

    lan3

--- app.py
+++ app.py
@@ -10,9 +10,14 @@ mlab.connect()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # print(user_id)
+    if "logged_in" in session:
+        if session['logged_in'] == True:
+            return render_template('index2.html', full_name = session['user_name'])
+    else:
+        return render_template('index.html')
 
-
+#################### LOGIN #######################
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "GET":
@@ -28,9 +33,14 @@ def login():
             return "Sai usename or password"
         else:
             session['logged_in'] = True
-            session['user_id'] = str(users[0].id)
-            return "Vào trang cá nhân"
+            # session['user_id'] = str(users[0].id)
+            for user in users:
+                session['user_name'] = user['fname']
+            return redirect(url_for('index'))
 
+
+
+#################### SIGN-UP #########################
 @app.route('/sign-up', methods=["GET", "POST"])
 def sign_up():
     if request.method == "GET":
@@ -51,11 +61,54 @@ def sign_up():
 
         new_user.save()
         # sẽ cho redirect vào trang chủ luôn
-        return "Bạn đã đk thành công"
+        return redirect(url_for('individual'))
 
+######################## BLOG ###########################
 @app.route('/blog')
 def blog():
     return render_template('blog.html')
+
+
+######################### BMI ########################
+@app.route('/bmi', methods=["GET", "POST"])
+def bmi():
+    if request.method == "GET":
+        return render_template('check.html')
+    elif request.method == "POST":
+        form = request.form
+        weight = form['weight']
+        height = form['height']
+
+        height = int(height) / 100
+        bmi = int(weight) / (height ** 2)
+
+        if bmi < 18.5:
+            return render_template('underweight.html') 
+        elif 18.5 <= bmi < 25:
+            return render_template('normal.html')
+        elif 25 <= bmi < 30:
+            return render_template('overweight.html')
+        elif bmi > 30:
+            return render_template('obese.html') 
+
+############################ LOG-OUT #####################
+@app.route('/logout')
+def log_out():
+    del session['logged_in']
+    return redirect(url_for('index'))
+
+
+@app.route('/individual')
+def individual():
+    print(session)
+    if "logged_in" in session:
+        if session['logged_in'] == True:
+            return render_template('individual.html')
+        else:
+            return redirect(url_for('login'))
+    else:
+        return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
   app.run(debug=True)
